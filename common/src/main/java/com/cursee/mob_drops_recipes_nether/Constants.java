ffdcf22601ba72a7ca921e63ea6151c660fbf60d
jason13official <jason13official@gmail.com>
2025-07-02 14:59:21 -0400

    fix?

--- common/src/main/java/com/cursee/mob_drops_recipes_nether/Constants.java
+++ common/src/main/java/com/cursee/mob_drops_recipes_nether/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 
 	public static final String MOD_ID = "mob_drops_recipes_nether";
 	public static final String MOD_NAME = "Mob Drops Recipes Nether";
-	public static final String MOD_VERSION = "2.1.0";
+	public static final String MOD_VERSION = "2.1.1";
 	public static final String MC_VERSION_RAW = "[1.21.1]";
 	public static final Pair<String, String> PUBLISHER_AUTHOR = new Pair<String, String>("Lupin", "Jason13");
 	public static final Triplet<String, String, String> PRIMARY_CURSEFORGE_MODRINTH = new Triplet<String, String, String>("https://www.curseforge.com/members/lupin/projects", "https://www.curseforge.com/members/lupin/projects", "https://modrinth.com/user/Lupin/mods");
